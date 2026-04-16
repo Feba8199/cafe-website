22dc0ab31563c6a268f7ef9b46df403abd4d1efe
Feba8199 <feba.johny@gmail.com>
2026-04-16 16:40:23 +1200

    favicon and name

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export const metadata: Metadata = {
     'brunch Auckland',
   ],
   icons: {
-    icon: '/logo.jpeg',
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
   },
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,35 @@ import SocialsSection from '../components/home/SocialsSection';
 import MenuItemGallery from '../components/MenuItemGallery';
 import MapSection from '../components/home/MapSection';
 export default function HomePage() {
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'CafeOrCoffeeShop',
+    name: 'Destination Cafe',
+    url: 'https://destinationcafe.co.nz',
+    image: ['https://destinationcafe.co.nz/logo.jpeg'],
+    telephone: '+64 22 057 3327',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: '1 Marewa Road',
+      addressLocality: 'Greenlane',
+      addressRegion: 'Auckland',
+      postalCode: '1051',
+      addressCountry: 'NZ',
+    },
+    servesCuisine: ['Coffee', 'Brunch', 'Cafe Food'],
+  };
   return (
-    <main>
-      <HeroSection />
-      <SocialsSection />
-      <MenuItemGallery />
-      <MapSection />
-    </main>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+      />
+      <main>
+        <HeroSection />
+        <SocialsSection />
+        <MenuItemGallery />
+        <MapSection />
+      </main>
+    </>
   );
 }
